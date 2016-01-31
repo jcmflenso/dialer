6c9e9184a8ae00d975b014f48da9fe2dc7e1c39c
Julio Mino <jcm@localhost.localdomain>
2016-01-31 00:57:46 -0600

    Second version

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    times = 3
+    return "Hello World" * times
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
