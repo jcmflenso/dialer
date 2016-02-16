16c395ec9c96f5c21d0b074ab785ef04c616adf8
Julio Mino <jcm@localhost.localdomain>
2016-02-15 23:33:27 -0600

    Let's go./test.py  Version 0.0.1

---
+++ dialer.py
@@ -0,0 +1,21 @@
+#Dialer System  - Markleting Leaders
+#02/15/2016
+#0.0.1
+from flask import Flask, url_for, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    #return "Index Page"
+    return redirect(url_for('login'))
+
+@app.route('/')
+@app.route('/login')
+def login():
+    return "Welcome to Marketing Leaders"
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
