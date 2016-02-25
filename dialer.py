04f19e93319a372fdd7d9a160f44441bf3b49b10
Julio Mino <jcm@localhost.localdomain>
2016-02-25 01:02:50 -0600

    session and logo

--- dialer.py
+++ dialer.py
@@ -2,7 +2,7 @@
 #02/15/2016
 #0.0.7
 
-from flask import Flask, url_for, request, redirect, render_template, flash, make_response
+from flask import Flask, url_for, request, redirect, render_template, flash, session
 
 app = Flask(__name__)
 
@@ -12,9 +12,8 @@ def login():
     if request.method == 'POST':
         if valid_login(request.form.get('userid'),request.form.get('passwd')):
             flash('Succesfully logged in')
-            response = make_response(redirect(url_for('welcome')))
-            response.set_cookie('userid', request.form.get('userid'))
-            return response
+            session['userid'] = request.form.get('userid')
+            return redirect(url_for('welcome'))
         else:
             error = 'Incorrect username and password'
 
@@ -22,9 +21,8 @@ def login():
 
 @app.route('/logout')
 def logout():
-    response = make_response(redirect(url_for('login')))
-    response.set_cookie('userid','',expires=0)
-    return response
+    session.pop('userid',None)
+    return redirect(url_for('login'))
 
 
 def valid_login(userid,passwd):
@@ -37,16 +35,13 @@ def valid_login(userid,passwd):
 
 @app.route('/')
 def welcome():
-    userid = request.cookies.get('userid')
+    userid = session.get('userid')
     if userid:
         return render_template('welcome.html', userid=userid)
     else:
         return redirect(url_for('login'))
 
-
-
-
 if __name__ == "__main__":
     app.debug = True
-    app.secret_key = 'flenso'
+    app.secret_key = '\xa2/Y\xf2\x15?q\xf4\xbe"\xb1\x91\x93\x8b\xad\n\xd0K\xed/8\x8d\\i'
     app.run()
