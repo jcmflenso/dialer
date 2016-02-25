a2ac71e5f5af671549bba04e17ce1f41369f44b7
jcmflenso <jcm@thinkcbu.frontier.com>
2016-02-24 18:00:27 -0600

    cookies

--- dialer.py
+++ dialer.py
@@ -2,15 +2,7 @@
 #02/15/2016
 #0.0.6
 
-
-
-
-
-
-
-
-
-from flask import Flask, url_for, request, redirect, render_template, flash
+from flask import Flask, url_for, request, redirect, render_template, flash, make_response
 
 app = Flask(__name__)
 
@@ -26,11 +18,20 @@ def login():
     if request.method == 'POST':
         if valid_login(request.form['userid'],request.form['passwd']):
             flash('Succesfully logged in')
-            return redirect(url_for('welcome', userid=request.form.get('userid')))
+            response = make_response(redirect(url_for('welcome')))
+            response.set_cookie('userid', request.form['userid'])
+            return response
         else:
             error = 'Incorrect username and password'
 
     return render_template('index.html', error=error)
+
+@app.route('/logout')
+def logout():
+    response = make_response(redirect(url_for('login')))
+    response.set_cookie('userid','',expires=0)
+    return response
+
 
 def valid_login(userid,passwd):
     if userid == passwd:
@@ -38,9 +39,13 @@ def valid_login(userid,passwd):
     else:
         return False
 
-@app.route('/welcome/<userid>')
-def welcome(userid):
-    return render_template('welcome.html', userid=userid)
+@app.route('/')
+def welcome():
+    userid = request.cookies.get('userid')
+    if userid:
+        return render_template('welcome.html', userid=userid)
+    else:
+        return redirect(url_for('login'))
 
 
 
